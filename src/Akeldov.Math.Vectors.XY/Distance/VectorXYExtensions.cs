65ecc641ec5f4aed58692cf4f5023180eb5a89eb
Stanislav Akeldov <akeldovstas@gmail.com>
2025-10-19 19:15:25 +0300

    Added Distance Extensions

---
+++ src/Akeldov.Math.Vectors.XY/Distance/VectorXYExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Akeldov.Math.Vectors.XY
+{
+    public static partial class VectorXYExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Distance(this VectorXY a, VectorXY b)
+        {
+            float dx = a.X - b.X;
+            float dy = a.Y - b.Y;
+            return MathF.Sqrt(dx * dx + dy * dy);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Distance(this VectorXY a, VectorXYInt b)
+        {
+            float dx = a.X - b.X;
+            float dy = a.Y - b.Y;
+            return MathF.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
---
+++ src/Akeldov.Math.Vectors.XY/Distance/VectorXYIntExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Akeldov.Math.Vectors.XY
+{
+    public static partial class VectorXYIntExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Distance(this VectorXYInt a, VectorXYInt b)
+        {
+            float dx = (float)a.X - b.X;
+            float dy = (float)a.Y - b.Y;
+            return MathF.Sqrt(dx * dx + dy * dy);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Distance(this VectorXYInt a, VectorXY b)
+        {
+            float dx = a.X - b.X;
+            float dy = a.Y - b.Y;
+            return MathF.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
