023af792c8fb33e6a4e11e80da94bc6c075ec7e5
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2025-10-19 19:16:06 +0300

    Added Sum Extensions

---
+++ src/Akeldov.Math.Vectors.XY/Sum/IEnumerableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Akeldov.Math.Vectors.XY
+{
+    public static class IEnumerableExtensions
+    {
+        public static VectorXY Sum(this IEnumerable<VectorXY> vectors)
+        {
+            var res = VectorXY.Zero;
+
+            foreach (var vector in vectors)
+            {
+                res = res + vector;
+            }
+
+            return res;
+        }
+
+        public static VectorXY Average(this IEnumerable<VectorXY> vectors)
+        {
+            var res = VectorXY.Zero;
+            var count = 0;
+
+            foreach (var vector in vectors)
+            {
+                res = res + vector;
+                count++;
+            }
+
+            return res / count;
+        }
+    }
+}
