ae348ee32d75dbde0b5d2eed4fb84e3fa069ea0a
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2025-10-26 19:38:47 +0300

    Added Rotating Extensions

---
+++ src/Akeldov.Math.Vectors.XY/LinearTransformations/VectorXYExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Akeldov.Math.Vectors.XY
+{
+    public static partial class VectorXYExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY Rotate(this VectorXY point, float angleRad)
+        {
+            float cos = MathF.Cos(angleRad);
+            float sin = MathF.Sin(angleRad);
+
+            float x = point.X * cos - point.Y * sin;
+            float y = point.X * sin + point.Y * cos;
+
+            return new VectorXY(x, y);
+        }
+    }
+}
