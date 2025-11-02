d121e6b0c8015f832f3bca007a0529ea0bd6c6cf
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2025-11-02 22:50:59 +0300

    Added Hadamard Extensions

---
+++ src/Akeldov.Math.Vectors.XY/Hadamard/VectorXYExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Akeldov.Math.Vectors.XY
+{
+    public static partial class VectorXYExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY HadamardMultiply(this VectorXY a, VectorXY b)
+        {
+            return new VectorXY(a.X * b.X, a.Y * b.Y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY HadamardMultiply(this VectorXY a, VectorXYInt b)
+        {
+            return new VectorXY(a.X * b.X, a.Y * b.Y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY HadamardDivide(this VectorXY a, VectorXY b)
+        {
+            return new VectorXY(a.X / b.X, a.Y / b.Y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY HadamardDivide(this VectorXY a, VectorXYInt b)
+        {
+            return new VectorXY(a.X / b.X, a.Y / b.Y);
+        }
+    }
+}
---
+++ src/Akeldov.Math.Vectors.XY/Hadamard/VectorXYIntExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Akeldov.Math.Vectors.XY
+{
+    public static partial class VectorXYIntExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXYInt HadamardMultiply(this VectorXYInt a, VectorXYInt b)
+        {
+            return new VectorXYInt(a.X * b.X, a.Y * b.Y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY HadamardMultiply(this VectorXYInt a, VectorXY b)
+        {
+            return new VectorXY(a.X * b.X, a.Y * b.Y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY HadamardDivide(this VectorXYInt a, VectorXY b)
+        {
+            return new VectorXY(a.X / b.X, a.Y / b.Y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY HadamardDivide(this VectorXYInt a, VectorXYInt b)
+        {
+            return new VectorXY(a.X / b.X, a.Y / b.Y);
+        }
+    }
+}
