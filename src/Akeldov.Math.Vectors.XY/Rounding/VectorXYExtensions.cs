544a4e979c7e3d3d5f673231607f434464d40207
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2025-11-09 19:41:37 +0300

    Added Rounding Extensions

---
+++ src/Akeldov.Math.Vectors.XY/Rounding/VectorXYExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Akeldov.Math.Vectors.XY
+{
+    public static partial class VectorXYExtensions
+    {
+        public static VectorXY[] Round(this VectorXY[] vectors, int digits)
+        {
+            var res = new VectorXY[vectors.Length];
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                res[i] = vectors[i].Round(digits);
+            }
+            return res;
+        }
+
+        public static VectorXY Round(this VectorXY vector, int digits)
+        {
+            return new VectorXY(System.MathF.Round(vector.X, digits), System.MathF.Round(vector.Y, digits));
+        }
+    }
+}
