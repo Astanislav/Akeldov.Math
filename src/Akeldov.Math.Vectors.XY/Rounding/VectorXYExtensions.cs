fcc61ffd787d35568eb66018346a046258b67216
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2025-11-09 19:44:40 +0300

    Added Floor and Ceiling Extensions

--- src/Akeldov.Math.Vectors.XY/Rounding/VectorXYExtensions.cs
+++ src/Akeldov.Math.Vectors.XY/Rounding/VectorXYExtensions.cs
@@ -12,9 +12,39 @@
             return res;
         }
 
+        public static VectorXY[] Floor(this VectorXY[] vectors, int digits)
+        {
+            var res = new VectorXY[vectors.Length];
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                res[i] = vectors[i].Floor(digits);
+            }
+            return res;
+        }
+
+        public static VectorXY[] Ceiling(this VectorXY[] vectors, int digits)
+        {
+            var res = new VectorXY[vectors.Length];
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                res[i] = vectors[i].Ceiling(digits);
+            }
+            return res;
+        }
+
         public static VectorXY Round(this VectorXY vector, int digits)
         {
             return new VectorXY(System.MathF.Round(vector.X, digits), System.MathF.Round(vector.Y, digits));
+        }
+
+        public static VectorXY Floor(this VectorXY vector, int digits)
+        {
+            return new VectorXY(System.MathF.Floor(vector.X), System.MathF.Floor(vector.Y));
+        }
+
+        public static VectorXY Ceiling(this VectorXY vector, int digits)
+        {
+            return new VectorXY(System.MathF.Ceiling(vector.X), System.MathF.Ceiling(vector.Y));
         }
     }
 }
