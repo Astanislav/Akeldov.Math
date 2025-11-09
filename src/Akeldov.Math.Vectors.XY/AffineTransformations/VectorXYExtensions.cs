ff6573a13d546123d67871f6719ab1cff2b2769a
Stanislav Akeldov <akeldovstas@gmail.com>
2025-11-09 20:08:36 +0300

    Added AffineTransformations Extensions

---
+++ src/Akeldov.Math.Vectors.XY/AffineTransformations/VectorXYExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Akeldov.Math.Vectors.XY
+{
+    public static partial class VectorXYExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY Transform(this VectorXY point, float angleRad, VectorXY offset)
+        {
+            var rotated = point.Rotate(angleRad);
+            return rotated + offset;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY Transform(this VectorXY point, float angleRad, VectorXYInt offset)
+        {
+            var rotated = point.Rotate(angleRad);
+            return rotated + offset;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY Transform(this VectorXY point, float scaleFactor, float angleRad, VectorXY offset)
+        {
+            var scaled = point * scaleFactor;
+            var rotated = scaled.Rotate(angleRad);
+            return rotated + offset;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY Transform(this VectorXY point, float scaleFactor, float angleRad, VectorXYInt offset)
+        {
+            var scaled = point * scaleFactor;
+            var rotated = scaled.Rotate(angleRad);
+            return rotated + offset;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY Rotate(this VectorXY point, VectorXY pivot, float angleRad)
+        {
+            float cos = MathF.Cos(angleRad);
+            float sin = MathF.Sin(angleRad);
+
+            VectorXY offset = point - pivot;
+
+            float rotatedX = offset.X * cos - offset.Y * sin;
+            float rotatedY = offset.X * sin + offset.Y * cos;
+
+            return new VectorXY(rotatedX, rotatedY) + pivot;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorXY Rotate(this VectorXY point, VectorXYInt pivot, float angleRad)
+        {
+            float cos = MathF.Cos(angleRad);
+            float sin = MathF.Sin(angleRad);
+
+            VectorXY offset = point - pivot;
+
+            float rotatedX = offset.X * cos - offset.Y * sin;
+            float rotatedY = offset.X * sin + offset.Y * cos;
+
+            return new VectorXY(rotatedX, rotatedY) + pivot;
+        }
+    }
+}
