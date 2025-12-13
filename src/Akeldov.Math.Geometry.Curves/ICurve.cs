151b71e8215d20ccde5be5bb2bd8a04e8992368b
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2025-12-13 23:25:57 +0300

    Added Math.Geometry.Curves project

---
+++ src/Akeldov.Math.Geometry.Curves/ICurve.cs
@@ -0,0 +1,11 @@
+﻿using Akeldov.Math.Vectors.XY;
+
+namespace Akeldov.Math.Geometry.Curves
+{
+    public interface ICurve
+    {
+        List<VectorXY> RayIntersections(Ray ray);
+
+        float Distance(VectorXY point);
+    }
+}
