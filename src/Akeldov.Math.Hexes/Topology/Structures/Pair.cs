4164d510a1e23dff744e0ddede05b1b41f629432
Stanislav Akeldov <23309915+Astanislav@users.noreply.github.com>
2026-05-14 23:25:24 +0300

    feat(hexes): add pair and triplet structures

---
+++ src/Akeldov.Math.Hexes/Topology/Structures/Pair.cs
@@ -0,0 +1,21 @@
+﻿namespace Akeldov.Math.Hexes.Topology
+{
+    public readonly struct Pair<T>
+    {
+        public Pair(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public T Left { get; }
+
+        public T Right { get; }
+
+        public void Deconstruct(out T left, out T right)
+        {
+            left = Left;
+            right = Right;
+        }
+    }
+}
---
+++ src/Akeldov.Math.Hexes/Topology/Structures/Triplet.cs
@@ -0,0 +1,25 @@
+﻿namespace Akeldov.Math.Hexes.Topology
+{
+    public readonly struct Triplet<T>
+    {
+        public Triplet(T main, T left, T right)
+        {
+            Main = main;
+            Left = left;
+            Right = right;
+        }
+
+        public T Main { get; }
+
+        public T Left { get; }
+
+        public T Right { get; }
+
+        public void Deconstruct(out T main, out T left, out T right)
+        {
+            main = Main;
+            left = Left;
+            right = Right;
+        }
+    }
+}
